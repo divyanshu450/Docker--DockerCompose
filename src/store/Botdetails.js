314eafd21f070b0f8628a80ab0926958c1176580
Divyanshu Kumar <divyanshu.kumar450@gmail.com>
2021-10-01 13:33:05 +0530

    Changes made

--- src/store/Botdetails.js
+++ src/store/Botdetails.js
@@ -1,26 +1,44 @@
-import {createStore} from 'redux'
-
+import { createStore } from "redux";
 
 const Data = {
-    bots: [
-        { "id": 1, "bot": "Hot Bot", "description": "Hot bot is low volatility portfolio of growth stocks selected based on our proprietary metrics", "index_value": 289.34, "cagr": 34 },
-        { "id": 2, "bot": "Cool Bot", "description": "Cool bot is low volatility portfolio of growth stocks selected based on our proprietary metrics", "index_value": 439.34, "cagr": 28 },
-        { "id": 3, "bot": "Options Bot", "description": "Options bot is low volatility portfolio of growth stocks selected based on our proprietary metrics", "index_value": 139.34, "cagr": 42 }
-    ],
-    count:0
-}
+  bots: [
+    {
+      id: 1,
+      bot: "Hot Bot",
+      description:
+        "Hot bot is low volatility portfolio of growth stocks selected based on our proprietary metrics",
+      index_value: 289.34,
+      cagr: 34,
+    },
+    {
+      id: 2,
+      bot: "Cool Bot",
+      description:
+        "Cool bot is low volatility portfolio of growth stocks selected based on our proprietary metrics",
+      index_value: 439.34,
+      cagr: 28,
+    },
+    {
+      id: 3,
+      bot: "Options Bot",
+      description:
+        "Options bot is low volatility portfolio of growth stocks selected based on our proprietary metrics",
+      index_value: 139.34,
+      cagr: 42,
+    },
+  ],
+  count: 0,
+};
 
+const Botdetails = (state = Data, action) => {
+  switch (action.type) {
+    case "ADD":
+      return { bots: [...state.bots], count: state.count + 1 };
 
+    default:
+      return state;
+  }
+};
 
- const Botdetails = (state = Data, action) => {
-    switch(action.type){
-        case 'ADD':
-            return({ bots:[...state.bots], count:state.count+1 })
-
-            default:
-                return state
-    }
- }
-
-const store=createStore(Botdetails);
+const store = createStore(Botdetails);
 export default store;
